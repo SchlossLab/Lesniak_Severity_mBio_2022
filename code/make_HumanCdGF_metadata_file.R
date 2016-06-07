97fcebe0f4cc7c3dc6c25a9b41e821b7dc0dad91
Nicholas Lesniak <NLesniak@umich.edu>
2016-06-07 12:35:17 -0400

    All processing of meta&shared move to make_meta_file.R

--- code/make_HumanCdGF_metadata_file.R
+++ code/make_HumanCdGF_metadata_file.R
@@ -9,20 +9,37 @@
 ###################
 setwd("~/Documents/Github/Schubert_humanCdGF_XXXX_2016")
 
-human_GF_exp <- read.table('data/raw/humanGF_ids.txt', sep='\t',header = T)
-human_GF_tox <- read.table('data/raw/Alyx_Humice_toxinassay_results.txt',sep='\t',header=T)
-human_GF_IDs <- read.table('data/raw/MIMARKS_cdclinical.txt',sep='\t',header=T)
+human_GF_mouse <- read.table('data/raw/humanGF_ids.txt', sep='\t',header = T)
+human_GF_toxin <- read.table('data/raw/Alyx_Humice_toxinassay_results.txt',sep='\t',header=T)
+human_GF_clinical <- read.table('data/raw/MIMARKS_cdclinical.txt',sep='\t',header=T)
+shared_file <- read.table('data/mothur/gf_cdiff.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.an.unique_list.0.03.subsample.shared', sep='\t',header = T)
+
+#subset df to include only sample and otus
+shared_file <- shared_file[,!colnames(shared_file) %in% c("label",'numOtus')]
 
 #Subset the human source metadata dataframe to a data frame of only samples used in study
 #   Subset sources to only ones used in study
-human_source_data <- human_GF_IDs[human_GF_IDs$sample_id %in% human_GF_exp$human_source,]
+human_GF_clinical <- human_GF_clinical[human_GF_clinical$sample_id %in% human_GF_mouse$human_source,]
 #   Subset data columns to remove columns with same information
-human_source_data <- human_source_data[,sapply(human_source_data, function(col) length(unique(col)))>1]
+human_GF_clinical <- human_GF_clinical[,sapply(human_GF_clinical, function(col) length(unique(col)))>1]
+#   Remove information from clinical seq and redundant columns
+human_GF_clinical <- human_GF_clinical[,!colnames(human_GF_clinical) %in% 
+                                         c("feature",'lib_reads_seqd','sfffile_id','mid')]
+# Remove file names
+human_GF_mouse <- human_GF_mouse[,!colnames(human_GF_mouse) %in% 'file']
 
 #Combine Toxin, Exp, and metadata
-human_GF_exp_metadata <- merge(human_GF_exp, human_GF_tox,by.x='group',by.y='Cage_Mouse',
+human_CdGF_metadata <- merge(human_GF_mouse, human_GF_toxin,by.x='group',by.y='Cage_Mouse',
                                incomparables=NA, all.x=T)
-human_GF_metadata <- merge(human_GF_exp_metadata,human_source_data,by.x='human_source',by.y='sample_id',
+human_CdGF_metadata <- merge(human_CdGF_metadata,human_GF_clinical,by.x='human_source',by.y='sample_id',
                            invcomparables=NA,all.x=T)
+
+#include only samples with both metadata and OTU data
+shared_file <- shared_file[shared_file$Group %in% human_CdGF_metadata$group,]
+human_CdGF_metadata <- human_CdGF_metadata[human_CdGF_metadata$group %in% shared_file$Group,]
+human_CdGF_metadata <- human_CdGF_metadata[order(human_CdGF_metadata$group),]
+
+
 #output metadata file
-write.table(human_GF_metadata, file='data/process/human_GF_metadata.txt',quote=F,sep='\t')
+write.table(human_CdGF_metadata, file='data/process/human_CdGF_metadata.txt',quote=F,sep='\t')
+write.table(shared_file, file='data/process/human_CdGF.an.unique_list.0.03.subsample.shared',quote=F,sep='\t')
