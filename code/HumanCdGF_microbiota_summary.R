ace58a56e59fba30816a1dec68ce76a5019c5bc7
Nicholas Lesniak <NLesniak@umich.edu>
2016-06-08 11:42:09 -0400

    Added function to convert taxonomy to df

--- code/HumanCdGF_microbiota_summary.R
+++ code/HumanCdGF_microbiota_summary.R
@@ -10,43 +10,51 @@
 setwd("~/Documents/Github/Schubert_humanCdGF_XXXX_2016")
 
 #read in files
-meta_file <- read.table('data/process/human_GF_metadata.txt', sep='\t',header = T)
-shared_file <- read.table('data/process/human_CdGF.an.unique_list.0.03.subsample.shared', sep='\t',header = T, row.names = 2)
+meta_file <- read.table('data/process/human_CdGF_metadata.txt', sep='\t',header = T, row.names = 2)
+shared_file <- read.table('data/process/human_CdGF.an.unique_list.0.03.subsample.shared', sep='\t',header = T, row.names=1)
 tax_file <- read.table('data/process/human_CdGF.an.unique_list.0.03.cons.taxonomy', sep='\t',header = T, row.names = 1)
 
-
-#Eliminate columns with label and numOtus
-shared_file <- shared_file[,!colnames(shared_file) %in% c("label",'numOtus')]
-#Create vector with number of seq per sample
-num_seq <- apply(shared_file, 1, sum)
 #Create df with relative abundances
-rel_abund <- 100*shared_file/num_seq
+rel_abund <- 100*shared_file/unique(apply(shared_file, 1, sum))
 
-#remove samples not followed in this study or sequenced
-rel_abund <- rel_abund[rownames(rel_abund) %in% meta_file$group,]
-meta_file <- meta_file[meta_file$group %in% rownames(rel_abund),]
-rownames(meta_file) <- meta_file[,2]
-meta_file <- meta_file[order(rownames(meta_file)),-2]
-
-#determine median abundances >1%
+#Create vector of OTUs with median abundances >1%
 med_rel_abund_cage <- aggregate(rel_abund, by=list(meta_file$cage_id),median)
-cage_IDs <- med_rel_abund_cage[,1]
-med_rel_abund_cage <- med_rel_abund_cage[,-1]
-otus_1 <- apply(med_rel_abund_cage, 2, max) > 1
-med_rel_abund_cage_1 <- med_rel_abund_cage[!cage_IDs=='inoculum',otus_1]
-num_otus <- sum(otus_1)
-#abundances of inoculum in OTUs >1%
-inoculum_rel_abund <- rel_abund[meta_file$cage_id=='inoculum',otus_1]
+cage_IDs <- med_rel_abund_cage[,"Group.1"]
+med_rel_abund_cage <- med_rel_abund_cage[,!colnames(med_rel_abund_cage) %in% 'Group.1']
+OTUs_1 <- apply(med_rel_abund_cage, 2, max) > 1
+OTU_list <- colnames(rel_abund)[OTUs_1]
 
-#get taxonomy
-taxonomy <- tax_file$Taxonomy
-  taxonomy <- gsub("\\(\\d*\\)",'',taxonomy)#elimnate values
-  taxonomy <- gsub(";unclassified", "", taxonomy) #eliminate unclassified
-  taxonomy <- gsub("/.*", "", taxonomy) #eliminate 
-  taxonomy <- gsub(";$", "", taxonomy) #eliminate end ;
-  taxonomy <- gsub("\"$", "", taxonomy) #eliminate end parenthesis
-  taxonomy <- gsub(".*;", "", taxonomy) #eliminate preceding text, except last
-  taxonomy <- gsub("^\"", "", taxonomy) #eliminate beginning parenthesis
-  names(taxonomy) <- rownames(tax_file)
-  taxonomy <- taxonomy[colnames(med_rel_abund_cage_1)] #only want phyla for OTUs >1%
+#df of OTUs with abundances >1% - by cage and inoculum
+rel_abund_cage <- rel_abund[!meta_file$cage_IDs=='inoculum',OTUs_1]
+inoculum_rel_abund <- rel_abund[meta_file$cage_id=='inoculum',OTUs_1]
 
+#get taxonomy - df with columns for each level - 
+# level - 1 (genus), 2 (family), 3 (order), 4 (class), 5 (phylum), 6 (kingdom)
+## - convert taxonomy text list to df, remove percentages
+## - subset df by desired tax level, then replace any unclassified with next level up
+get_tax <- function(tax_level=1){
+    if (tax_level %in% c(1:5)){
+      taxonomy <- tax_file[OTU_list,]
+      taxonomy <-  data.frame(do.call('rbind', strsplit(as.character(taxonomy$Taxonomy),';',fixed=TRUE)))
+      taxonomy <- data.frame(sapply(taxonomy,gsub,pattern="\\(\\d*\\)",replacement=""))
+              level <- 7-tax_level
+      tax_out <- as.character(taxonomy[,level])
+      for (i in level:2){
+        next_level <- i-1
+        tax_out[tax_out=='unclassified'] <- 
+          as.character(taxonomy[tax_out=='unclassified',next_level])
+      }
+      return(data.frame(tax= tax_out, row.names=OTU_list))
+    } else {print(
+      'Error: Level must be 1 (genus), 2 (family), 3 (order), 4 (class), 5 (phylum)')
+    }
+}
+
+taxonomy <- get_tax(1)
+
+#Create median rel abundance and IQRs
+median_rel_abund <- aggregate()
+
+low_QTR
+
+up_QTR
