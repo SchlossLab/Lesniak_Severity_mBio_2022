d3dd2231d54c6d37c332942f9706fc0d577c8968
Kaitlin <kjflynn06@gmail.com>
2016-06-27 15:04:56 -0400

    processes data for d0 analysis

--- code/mouse_d0_community.R
+++ code/mouse_d0_community.R
@@ -1,9 +1,84 @@
-#code to address issue 2
+##GET NEW SHARED FILE !!!
+
+##what bacteria were present in the mice on day 0?
 
 setwd("~/Documents/Schloss_Lab/Schubert_humanCdGF_XXXX_2016")
 
 #import Nick's awesome combined metadata file
-metadata <- read.table("data/process/human_CdGF_metadata.txt", header = TRUE, fill = TRUE)
+meta_file <- read.table("data/process/human_CdGF_metadata.txt", header = TRUE, sep='\t', fill = TRUE, row.names=2)
+shared_file <- read.table("data/process/human_CdGF.an.unique_list.0.03.subsample.shared", sep='\t', header = TRUE, row.names=1)
+tax_file <- read.table('data/process/human_CdGF.an.unique_list.0.03.cons.taxonomy', sep='\t',header = T, row.names = 1)
+
+####Use Nick's awesome code to get the OTU abundance table sorted out right###
+
+#Create df with relative abundances
+rel_abund <- 100*shared_file/unique(apply(shared_file, 1, sum))
+
+#Create vector of OTUs with median abundances >1%
+med_rel_abund_cage <- aggregate(rel_abund, by=list(meta_file$cage_id),median)
+cage_IDs <- med_rel_abund_cage[,"Group.1"]
+med_rel_abund_cage <- med_rel_abund_cage[,!colnames(med_rel_abund_cage) %in% 'Group.1']
+OTUs_1 <- apply(med_rel_abund_cage, 2, max) > 1
+OTU_list <- colnames(rel_abund)[OTUs_1]
+
+#df of OTUs with abundances >1% - by cage and inoculum
+rel_abund_cage <- rel_abund[!meta_file$cage_id=='inoculum',OTUs_1]
+inoculum_rel_abund <- rel_abund[meta_file$cage_id=='inoculum',OTUs_1]
+
+#df of OTUs w abundances >1% 
+rel_abund_d0 <- rel_abund[meta_file$day == 0, OTUs_1]
+
+#get taxonomy - df with columns for each level - 
+# level - 1 (genus), 2 (family), 3 (order), 4 (class), 5 (phylum), 6 (kingdom)
+## - convert taxonomy text list to df, remove percentages
+## - subset df by desired tax level, then replace any unclassified with next level up
+get_tax <- function(tax_level=1){
+  if (tax_level %in% c(1:5)){
+    taxonomy <- tax_file[OTU_list,]
+    taxonomy <-  data.frame(do.call('rbind', strsplit(as.character(taxonomy$Taxonomy),';',fixed=TRUE)))
+    taxonomy <- data.frame(sapply(taxonomy,gsub,pattern="\\(\\d*\\)",replacement=""))
+    level <- 7-tax_level
+    tax_out <- as.character(taxonomy[,level])
+    for (i in level:2){
+      next_level <- i-1
+      tax_out[tax_out=='unclassified'] <- 
+        as.character(taxonomy[tax_out=='unclassified',next_level])
+    }
+    return(data.frame(tax= tax_out, row.names=OTU_list))
+  } else {print(
+    'Error: Level must be 1 (genus), 2 (family), 3 (order), 4 (class), 5 (phylum)')
+  }
+}
 
 
+taxonomy_genus <- get_tax(1)
+taxonomy_phylum <- get_tax(5)
+
+sum_OTU_by_tax_level <- function(TAX_DF,OTU_DF){
+  tax_levels <- as.character(unique(TAX_DF$tax))
+  OUTPUT_DF <- data.frame(rep(0,length(rownames(OTU_DF))), row.names=rownames(OTU_DF))
+  for (i in 1:length(tax_levels)){
+    OTU_by_level <- rownames(TAX_DF)[TAX_DF$tax %in% tax_levels[i]]
+    if (length(OTU_by_level)>1){
+      level_column <- apply(OTU_DF[,names(OTU_DF)[names(OTU_DF) %in% OTU_by_level]],1,sum)
+    } else {
+      level_column <- OTU_DF[,names(OTU_DF)[names(OTU_DF) %in% OTU_by_level]]
+    }     
+    OUTPUT_DF[,i] <- level_column
+    colnames(OUTPUT_DF)[i] <- tax_levels[i]
+  }
+  return(OUTPUT_DF)
+}
+
+
+#get phylum level for D0 only
+
+phylum_d0 <- sum_OTU_by_tax_level(taxonomy_phylum, rel_abund_d0)
+
+
+#combine by cages
+
+#get OTU data from nicks shared file? or what
+
+#plot by donors/cages. bar plots? line plots of diversity? 
 
