5a46b9e34016c0c1f9e126901391178b0396cbac
Kaitlin <kjflynn06@gmail.com>
2016-09-12 17:00:03 -0400

    updated nmds code and files

--- code/inocula_nmds.R
+++ code/inocula_nmds.R
@@ -20,17 +20,38 @@ shared <- read.table('data/process/human_CdGF.an.unique_list.0.03.subsample.shar
 inocula <- rownames(meta_file)[meta_file$cage_id=='inoculum']
 inocula_shared <- shared[shared$sample_id %in% inocula,]
 
-#add back label and numOTUs column to subsetted shared file-- need this to run dist.shared
+#subset shared to be day 0 only- will use later for comparison 
+day0 <- rownames(meta_file)[meta_file$day==0]
+day0_shared <- shared[shared$sample_id %in% day0,]
 
+#add back label and numOTUs column to subsetted shared file-- need this to run dist.shared
 inocula_shared <- cbind(label=0.03, inocula_shared[1], numOTUs=2255, inocula_shared[2:ncol(inocula_shared)])
 colnames(inocula_shared)[2] <- 'Group'
 
+#for day 0 also
+day0_shared <- cbind(label=0.03, day0_shared[1], numOTUs=2255, day0_shared[2:ncol(day0_shared)])
+colnames(day0_shared)[2] <- 'Group'
+
+#combine these all to run as one file duhhhh
+inputd0_shared <- rbind(inocula_shared, day0_shared)
+
 #write file out so we can use it in mothur
 write.table(inocula_shared, file='data/process/inocula.shared', quote=F,sep='\t',row.names=F)
+write.table(day0_shared, file='data/process/day0.shared', quote=F,sep='\t',row.names=F)
+write.table(inputd0_shared, file='data/process/inputd0.shared', quote=F, sep='\t', row.names=F)
 
 #run dist.shared, nmds in mothur to get axes file
+#mothur ouput stats for combined file: 
+#Number of dimensions:	2
+#Lowest stress :	0.362683
+#R-squared for configuration:	0.380491
 
-nmds <- read.table(file='data/process/inocula.thetayc.0.03.lt.ave.nmds.axes', header = T)
+combo_nmds <- read.table(file='data/process/inputd0.thetayc.0.03.lt.nmds.axes', header = T)
+inoc_nmds <- read.table(file='data/process/inocula.thetayc.0.03.lt.nmds.axes', header = T)
+day0_nmds <- read.table(file='data/process/day0.thetayc.0.03.lt.nmds.axes', header = T)
+
+
+
 
 #need to color points by type 
 inocula_design <- read.table(file='data/raw/donor.design', header = F)
