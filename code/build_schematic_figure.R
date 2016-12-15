77581fd948ffeb899ea3628d4bad6c150a2fad97
Kaitlin <kjflynn06@gmail.com>
2016-12-15 11:27:30 -0500

    figure 1 schematic fixed

--- code/build_schematic_figure.R
+++ code/build_schematic_figure.R
@@ -1,52 +1,17 @@
 # Builds figure 1
 # Experimental timeline code from Matt Jenior
-
-# Load dependencies 
-deps <- c('shape', 'plotrix', 'wesanderson');
+# Load dependencies
+deps <- c('shape', 'wesanderson');
 for (dep in deps){
   if (dep %in% installed.packages()[,"Package"] == FALSE){
     install.packages(as.character(dep), quiet=TRUE);
   } 
   library(dep, verbose=FALSE, character.only=TRUE)
 }
+#------------------------------------------------------------------------------------------------------------------------------------#
+# Import, analyze and clean data for figures before plotting
 
-#-------------------------------------------------------------------------------------------------------------------------------------#
-
-plot_file <- '~/Documents/Schloss_Lab/Schubert_humanCdGF_XXXX_2016/results/figures/figure_1.pdf'
-pdf(file=plot_file, width=9, height=9)
-
-# Create layout for multi-plot
-layout(mat=matrix(c(1,
-                    2,
-                    3,
-                    4,
-                    5), nrow=5, ncol=1, byrow=TRUE))
-
-# Figure 1A - experimental timeline code 
-#-------------------------------------------------------------------------------------------------------------------------------------#
-
-#---------Option 1, text based timeline---------#
-plot(0, type='n', axes=F, xlab='', ylab='', xlim=c(-5,5), ylim=c(-2,2)) # Empty plot
-Arrows(x0=-4, y0=0, x1=3, y1=0, lwd=4, arr.type='triangle', arr.length=0.6, arr.width=0.3)
-segments(x0=c(-4,-0.5,2), y0=c(0.5,0.5,0.5), x1=c(-4,-0.5,2), y1=c(-0.5,-0.5,-0.5), lwd=4)
-segments(x0=c(-3.5,-3,-2.5,-2,-1.5, -1), y0=c(0.25,0.25,0.25,0.25,0.25), x1=c(-3.5,-3,-2.5,-2,-1.5, -1), y1=c(-0.25,-0.25,-0.25,-0.25,-0.25), lwd=2)
-segments(x0=c(0,0.5,1,1.5), y0=c(0.25,0.25,0.25,0.25,0.25), x1=c(0,0.5,1,1.5), y1=c(-0.25,-0.25,-0.25,-0.25,-0.25), lwd=2)
-text(x=c(-4,-0.5, 2), y=c(-0.8,-0.8, -0.8), c('Day -14', 'Day 0', 'Day 10'), cex=1)
-text(x=c(-3.35,-0.5, 2), y=c(0.7,0.7, 0.7), c('Gavage human stool', 'Gavage C. difficile', 'Euthanize'), cex=1)
-text(x=-4.5, y=0, 'A', cex=1.5, font=2)
-
-#---------Option 2, text based timeline---------#
-
-
-#------------Option 3, rectangles timeline------#
-
-#-----------Option 4, mouse timeline--------#
-
-#----------------------------#
-#-------------------------------------------------------------------------------------------------------------------------------------#
-
-#Figure 1B - nmds of donor stool prior to inoculation 
-
+#Figure 1B data
 #read in files
 meta_file <- read.table('data/process/human_CdGF_metadata.txt', sep='\t',header = T, row.names = 2)
 shared <- read.table('data/process/human_CdGF.an.unique_list.0.03.subsample.shared', sep='\t',header=T)
@@ -84,26 +49,8 @@ inocula_meta_design <- read.table(file='data/raw/donor_meta.design', header = F)
 inocula_meta_nmds <- merge(inoc_nmds, inocula_meta_design, by.x='group', by.y='V1')
 colnames(inocula_meta_nmds)[4] <- "Disease_status"
 
-#build NMDS plot
-plot(inocula_meta_nmds$axis1, inocula_meta_nmds$axis2, xlab="NMDS Axis 1", ylab= "NMDS Axis 2")
-no_d <- inocula_meta_nmds[grep('No_diarrhea', inocula_meta_nmds$Disease_status), c(2,3)]
-diarrhea <- inocula_meta_nmds[grep('Diarrhea', inocula_meta_nmds$Disease_status), c(2,3)]
-cdiff <- inocula_meta_nmds[grep('C._difficile', inocula_meta_nmds$Disease_status), c(2,3)]
 
-points(no_d, pch=16, col = "blue")
-points(diarrhea, pch=16, col = "yellow2")
-points(cdiff, pch=16, col = "black")
-
-legend <- c("No diarrhea", "Diarrhea", "C. difficile")
-legend(x="topright", legend, col = c("blue", "yellow2", "black"), pch=16, cex=0.8)
-
-#put labels outside of plot
-#text(x=-0.3, y=0.7, 'B', cex=1.5, font=2)
-#will need to do that ADONIS here for stats
-
-#-----------------------------------------------------------------------------------------------------#
-#Figure 1C - NMDS of mice/cages on day 1 
-
+#Figure 1C data
 #prepare files and run on mothur
 #subset shared to be day 0 only 
 day0 <- rownames(meta_file)[meta_file$day==0]
@@ -130,6 +77,66 @@ day0_donor_nmds <- merge(day0_donor_nmds, d0_outcome, by.x='group', by.y='V1')
 names(day0_donor_nmds)[4] <- "Donor"
 names(day0_donor_nmds)[5] <- "Outcome"
 
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+# Set up multi-panel figure
+plot_file <- '~/Documents/Schloss_Lab/Schubert_humanCdGF_XXXX_2016/results/figures/figure_test.pdf'
+pdf(file=plot_file, width=9, height=9)
+layout(matrix(c(1,
+                2,
+                3), 
+              nrow=3, ncol=1, byrow = TRUE))
+
+# Figure 1A - experimental timeline code 
+#-------------------------------------------------------------------------------------------------------------------------------------#
+#---------Option 1, text based timeline---------#
+plot(0, type='n', axes=F, xlab='', ylab='', xlim=c(-5,5), ylim=c(-2,2)) # Empty plot
+Arrows(x0=-4, y0=0, x1=3, y1=0, lwd=4, arr.type='triangle', arr.length=0.6, arr.width=0.3)
+segments(x0=c(-4,-0.5,2), y0=c(0.5,0.5,0.5), x1=c(-4,-0.5,2), y1=c(-0.5,-0.5,-0.5), lwd=4)
+segments(x0=c(-3.5,-3,-2.5,-2,-1.5, -1), y0=c(0.25,0.25,0.25,0.25,0.25), x1=c(-3.5,-3,-2.5,-2,-1.5, -1), y1=c(-0.25,-0.25,-0.25,-0.25,-0.25), lwd=2)
+segments(x0=c(0,0.5,1,1.5), y0=c(0.25,0.25,0.25,0.25,0.25), x1=c(0,0.5,1,1.5), y1=c(-0.25,-0.25,-0.25,-0.25,-0.25), lwd=2)
+text(x=c(-4,-0.5, 2), y=c(-0.8,-0.8, -0.8), c('Day -14', 'Day 0', 'Day 10'), cex=1)
+text(x=c(-3.35,-0.5, 2), y=c(0.7,0.7, 0.7), c('Gavage human stool', 'Gavage C. difficile', 'Euthanize'), cex=1)
+text(x=-4.5, y=0, 'A', cex=1.5, font=2)
+
+#---------Option 2, text based timeline---------#
+
+
+#------------Option 3, rectangles timeline------#
+
+#-----------Option 4, mouse timeline--------#
+
+#----------------------------#
+#-------------------------------------------------------------------------------------------------------------------------------------#
+
+#Figure 1B - nmds of donor stool prior to inoculation 
+par(las=1, mar=c(0.7,4,0.7,1), mgp=c(2.5,0.7,0))
+
+
+#build NMDS plot
+plot(inocula_meta_nmds$axis1, inocula_meta_nmds$axis2, xlab="NMDS Axis 1", ylab= "NMDS Axis 2")
+no_d <- inocula_meta_nmds[grep('No_diarrhea', inocula_meta_nmds$Disease_status), c(2,3)]
+diarrhea <- inocula_meta_nmds[grep('Diarrhea', inocula_meta_nmds$Disease_status), c(2,3)]
+cdiff <- inocula_meta_nmds[grep('C._difficile', inocula_meta_nmds$Disease_status), c(2,3)]
+
+points(no_d, pch=16, col = "blue")
+points(diarrhea, pch=16, col = "yellow2")
+points(cdiff, pch=16, col = "black")
+
+legend <- c("No diarrhea", "Diarrhea", "C. difficile")
+legend(x="topright", legend, col = c("blue", "yellow2", "black"), pch=16, cex=0.8)
+
+#put labels outside of plot
+#text(x=-0.3, y=0.7, 'B', cex=1.5, font=2)
+#will need to do that ADONIS here for stats
+
+#-----------------------------------------------------------------------------------------------------#
+#Figure 1C - NMDS of mice/cages on day 1 
+par(las=1, mar=c(0.7,4,0.7,1), mgp=c(2.5,0.7,0))
+
+
 #plot
 colors <- c("dodgerblue2","#E31A1C", # red
             "green4",
@@ -142,22 +149,21 @@ colors <- c("dodgerblue2","#E31A1C", # red
             "#FDBF6F", # lt orange
             "gray70", "khaki2",
             "maroon","orchid1")
-plot(day0_donor_nmds$axis1, day0_donor_nmds$axis2, xlab='NMDS Axis 1', ylab='NMDS Axis 2', col=colors[as.numeric(day0_donor_nmds$Donor)], pch=c(16,17)[as.numeric(day0_donor_nmds$Outcome)])
+plot(day0_donor_nmds$axis1, day0_donor_nmds$axis2, xlab='NMDS Axis 1', ylab='NMDS Axis 2', col=wes_colors[as.numeric(day0_donor_nmds$Donor)], pch=c(1,2)[as.numeric(day0_donor_nmds$Outcome)], lwd = 3)
 
-par(mar=c(5,5,5,5), xpd=TRUE)
+#par(mar=c(5,5,5,5), xpd=TRUE)
 #just make the legend mild and severe 
 legend <- c("Mild", "Severe")
-legend(x="topright", legend, pch=c(16,17), cex=0.8)
+legend(x="topright", legend, pch=c(1,2), cex=1)
 
 
-#now sew together plots into one figure and save as pdf 
+#-------------------------------------------------------------------------------------------------------------------------------------#
 
 #Clean up
-dev.off()
-rm(plot_file)
 for (dep in deps){
   pkg <- paste('package:', dep, sep='')
   detach(pkg, character.only = TRUE)
 }
 rm(dep, deps, pkg)
+dev.off()
 gc()
