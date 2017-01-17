37d26d0260e8d211b9acdb5ae0d7836c3db4e293
Kaitlin Flynn <kaitlinflynn@lesniak-imac.local>
2017-01-17 10:35:54 -0500

    updated toxin code

--- code/toxin.R
+++ code/toxin.R
@@ -18,3 +18,10 @@ sampleID_bins <- read.table('data/process/sampleID_bins.design', header=T)
 toxin_file <- separate(toxin_file, Cage_Mouse, sep="-D", into = c("mouse_ID", "day"))
 sampleID_bins <- separate(sampleID_bins, sample_ID, sep='-D', into = c("mouse_ID", "day"))
 mouse_toxin <- merge(toxin_file, sampleID_bins, by.x="mouse_ID", by.y="mouse_ID")
+mouse_toxin <- merge(histo_file, mouse_toxin, by.x="mouse_id", by.y="mouse_ID")
+
+ggplot(mouse_toxin, aes(severity, Log_repiricoal_dilution)) + geom_jitter()
+
+ggplot(mouse_toxin, aes(summary_score.y, Log_repiricoal_dilution)) + geom_jitter(aes(color=severity)) +theme_bw() + ggtitle("toxin amounts by severity score")
+
+ggplot(mouse_toxin, aes(human_source, Log_repiricoal_dilution)) + geom_jitter(aes(color=severity)) + theme_bw() + ggtitle("toxin amounts by human source")
