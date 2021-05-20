9d80fe5c4b0b5f2a2dfb22f69cd9e3cbc15f392b
Nick Lesniak <NLesniak@umich.edu>
2021-05-20 14:10:14 -0400

    create script to setup data to run in ml model

---
+++ code/setup_ml_data.R
@@ -0,0 +1,104 @@
+################################################################################
+#
+# setup_ml_data.R
+#
+# This script creates the files to be used to run with mikropml
+# 	outputs a dataframe with the outcome and associated features
+#
+# Dependencies...
+# * data/process/metadata_tidy.tsv
+# * data/process/toxin_tidy.tsv
+# * data/process/histology_tidy.tsv
+# * data/mothur/sample.final.0.03.subsample.shared
+#
+# Output...
+# * data/process/ml_data/same_day_toxin.tsv
+# * data/process/ml_data/day_0_predict_future_toxin.tsv
+# * data/process/ml_data/day_0_moribund.tsv
+# * data/process/ml_data/day_10_histology.tsv
+#
+################################################################################
+
+# load packages
+library('tidyverse')
+
+# read in data
+metadata <- read_tsv('data/process/metadata_tidy.tsv',
+					 col_type = 'cccccDddddDcdddddcdl') %>% 
+	filter(cdiff_strain == 431) %>% 
+	mutate(cdiff_cfu = ifelse(cdiff_cfu == 0, log10(60), log10(cdiff_cfu)))
+
+toxin <- read_tsv('data/process/toxin_tidy.tsv',
+				  col_type = 'cccccdc') %>% 
+	mutate(toxin = ifelse(Log_repiricoal_dilution > 1, 'present', 'absent'))
+
+histology <- read_tsv('data/process/histology_tidy.tsv',
+					  col_type = 'ccccdddcdcc') %>% 
+	mutate(hist_score = case_when(summary_score < 5 ~ 'low',
+								  summary_score > 5 ~ 'high',
+								  summary_score == 5 ~ 'mid',
+								  T ~ 'NA'))
+
+shared <- read_tsv('data/mothur/sample.final.0.03.subsample.shared',
+         col_types = cols(.default = col_double(),
+                          Group = col_character())) %>% 
+  mutate(Group = ifelse(grepl('_D|DA', Group), Group,
+      gsub('D', '_D', Group))) %>% 
+  select(-label, -numOtus)
+
+
+
+# setup data to predict toxin level (microbiome effect on toxin)
+#	from day of sample
+same_day_toxin_df <- toxin %>% 
+	select(group, toxin) %>% 
+	inner_join(shared, by = c('group' = 'Group')) %>% 
+	left_join(select(metadata, group, cdiff_cfu), by = c('group')) %>% 
+	select(-group)
+write_tsv(same_day_toxin_df, 'data/process/ml_data/same_day_toxin.tsv')	
+
+#	from day 0?
+day_0_predict_future_toxin_df <- toxin %>% 
+	group_by(mouse_id) %>% 
+	summarise(toxin = max(Log_repiricoal_dilution) > 1) %>% 
+	left_join(select(metadata, mouse_id, day, group), 
+			  by = c('mouse_id')) %>% 
+	filter(day == 0) %>% 
+	inner_join(shared, by = c('group' = 'Group')) %>% 
+	select(-mouse_id, -day, -group)
+write_tsv(day_0_predict_future_toxin_df, 'data/process/ml_data/day_0_predict_future_toxin.tsv')	
+
+# setup data to predict moribunidty (microbiome, cdiff, toxin)
+toxin_summary <- toxin %>% 
+	group_by(mouse_id) %>% 
+	summarise(toxin_presence = max(Log_repiricoal_dilution) > 1,
+			  max_toxin = max(Log_repiricoal_dilution),
+			  median_toxin = median(Log_repiricoal_dilution))
+cfu_summary <- metadata %>% 
+	group_by(mouse_id, early_euth) %>% 
+	summarise(max_cfu = max(cdiff_cfu),
+			  median_cfu = median(cdiff_cfu))
+day_0_moribund <- metadata %>% 
+	select(group, mouse_id, day) %>% 
+	filter(day == 0) %>% 
+	left_join(cfu_summary, by = c('mouse_id')) %>% 
+	left_join(toxin_summary, by = c('mouse_id')) %>% 
+	inner_join(shared, by = c('group' = 'Group')) %>% 
+	mutate(early_euth = ifelse(early_euth, 'severe', 'mild')) %>% 
+	select(-mouse_id, -day, -group) %>% 
+	relocate(early_euth)
+write_tsv(day_0_moribund, 'data/process/ml_data/day_0_moribund.tsv')	
+
+# setup data to predict histology severity of colonized mice (microbiome, cdiff, toxin)
+day_10_histology_df <- metadata %>% 
+	filter(day == 10,
+		early_euth == F) %>% 
+	select(group, cdiff_cfu, mouse_id) %>% 
+	left_join(toxin_summary, by = c('mouse_id')) %>% 
+	left_join(select(toxin, group, toxin_level = Log_repiricoal_dilution),
+		by = c('group')) %>% 
+	inner_join(shared, by = c('group' = 'Group')) %>% 
+	inner_join(select(histology, mouse_id, hist_score), by = c('mouse_id')) %>% 
+	select(-mouse_id, -group) %>% 
+	relocate(hist_score)
+write_tsv(day_10_histology_df, 'data/process/ml_data/day_10_histology.tsv')	
