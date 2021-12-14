d804dcdc387fb31a3d164ac8fb3ff76e830986f7
Nick Lesniak <NLesniak@umich.edu>
2021-12-14 16:24:07 -0500

    remove unused - inocula

--- code/make_files_file.R
+++ code/make_files_file.R
@@ -20,12 +20,6 @@ library(tidyverse)
 # read in the names of all fastq available from project
 fastq_list <- list.files(path = 'data/raw/', pattern = 'fastq')
 
-# create dataframe with human source names and numbers
-sources <- read_tsv('data/process/human_source_tidy.tsv',
-		col_type = cols(.default = col_character())) %>% 
-	mutate(source_number = gsub('DA0*','',sample_id)) %>% 
-	select(source = sample_id, source_number)
-
 # separate fastqs by sample type - mock, inoculum, experiment since each has a different name structure
 fastq_df <- tibble(fastq = fastq_list) %>% 
 	mutate(sample_names = gsub('_S.*', '', fastq), # remove sequencing file naming suffixes
@@ -39,23 +33,16 @@ experiment_fastqs <- fastq_df %>%
 	select(sample_names, fastq) %>% 
 	mutate(read = str_extract(fastq, 'R[12]')) %>%
 	spread(read, fastq, fill = 'NA')
-# format inoculum samples into files format and add DA0* prefix
-inoculum_fastqs <- fastq_df %>% 
-	filter(sample_type == 'inoculum') %>% 
-	left_join(sources, by = c('sample_names' = 'source_number')) %>% 
-	select(sample_names = source, fastq) %>% 
-	mutate(read = str_extract(fastq, 'R[12]')) %>%
-	spread(read, fastq, fill = 'NA')
 # format inoculum samples into files format and add S## suffix to differentiate mocks
 mock_fastqs <- fastq_df %>% 
-	filter(sample_type == 'mock') %>% 
+	filter(sample_names %in% c('mock', 'mock1', 'mock2', 'Mock1')) %>% 
 	mutate(sample_names = gsub('.*(S\\d{2,3}).*', 'mock_\\1', fastq)) %>% 
 	select(sample_names, fastq) %>% 
 	mutate(read = str_extract(fastq, 'R[12]')) %>%
 	spread(read, fastq, fill = 'NA')
 
 # replace all - with _  since mothur uses - to split sames
-files_file <- bind_rows(experiment_fastqs, inoculum_fastqs, mock_fastqs) %>% 
+files_file <- bind_rows(experiment_fastqs, mock_fastqs) %>% 
 	mutate(sample_names = gsub('-', '_', sample_names))
 
 # output files file
